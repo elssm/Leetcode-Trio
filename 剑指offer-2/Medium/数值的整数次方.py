0f6c49071f391395299e1aa9120c32179c346a6e
elssm <329847986@qq.com>
2021-11-15 15:13:30 +0800

    offer16 in 11.15

---
+++ 剑指offer-2/Medium/数值的整数次方.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / x * self.myPow(1 / x, -n - 1)
+        if n % 2:
+            return x * self.myPow(x * x, n / 2)
+        if not n % 2:
+            return self.myPow(x * x, n / 2)
